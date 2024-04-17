554a8129f0892fe28bdd75e4e73bbf65296eb308
Haza Shami <hazashami@gmail.com>
2024-04-16 22:04:44 -0700

    added @ang/fire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"reps-se","appId":"1:588742206464:web:57ef087666f7b5486a24e5","storageBucket":"reps-se.appspot.com","apiKey":"AIzaSyBt5bG7yX5UmNc4qmFRWtSGnH_yXz42bO8","authDomain":"reps-se.firebaseapp.com","messagingSenderId":"588742206464"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebaseConfig: {
+        apiKey: "AIzaSyBt5bG7yX5UmNc4qmFRWtSGnH_yXz42bO8",
+        authDomain: "reps-se.firebaseapp.com",
+        projectId: "reps-se",
+        storageBucket: "reps-se.appspot.com",
+        messagingSenderId: "588742206464",
+        appId: "1:588742206464:web:57ef087666f7b5486a24e5"
+    }
+};
