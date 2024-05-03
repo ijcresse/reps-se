683aeaed4884df92091dee151d8b6f5b224279c1
Ian Cresse <ijcresse@gmail.com>
2024-05-03 09:23:07 -0700

    added a collection stringbuilder to keep the strings in one place

--- src/app/util.ts
+++ src/app/util.ts
@@ -4,3 +4,48 @@ export class Util {
                 .replace(/\s/g, "");
     }
 }
+
+//builds paths to collections.
+//appends slash for you.
+export class CollectionPathBuilder {
+    coll: string[];
+    path: string[];
+
+    constructor() {
+        this.coll = [
+            'WorkoutTemplates',
+            'Workouts',
+            'Users',
+            'Instances'
+        ]
+        this.path = [];
+    }
+
+    history(): string {
+        return 'History';
+    }
+
+    template(): CollectionPathBuilder {
+        this.path.push(`${this.coll[0]}/`);
+        return this;
+    }
+
+    workout(templateId: string): CollectionPathBuilder {
+        this.path.push(`${templateId}/${this.coll[1]}`);
+        return this;
+    }
+
+    user(workoutId: string): CollectionPathBuilder {
+        this.path.push(`${workoutId}/${this.coll[2]}`);
+        return this;
+    }
+
+    instance(userId: string): CollectionPathBuilder {
+        this.path.push(`${userId}/${this.coll[3]}`);
+        return this;
+    }
+
+    toString() {
+        return this.path.join("");
+    }
+}
